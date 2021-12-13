39220ff90761d58958b6ea8797b5c68894dce3b6
Meredith Gallop <meredithcat93@gmail.com>
2021-12-13 08:23:36 +0000

    refactor app & pet to jsx

--- src/App.js
+++ src/App.js
@@ -1,29 +1,15 @@
-import React from "react";
 import ReactDOM from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel",
-    }),
-    React.createElement(Pet, {
-      name: "Sudo",
-      animal: "Dog",
-      breed: "Wheaten Terrier",
-    }),
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Luna" breed="Dog" breed="Havanese" />
+      <Pet name="Pepper" breed="Bird" breed="Cockatiel" />
+      <Pet name="Sudo" breed="Dog" breed="Wheaten Terrier" />
+    </div>
+  );
 };
 
-ReactDOM.render(
-  React.createElement(App, {}, null),
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
